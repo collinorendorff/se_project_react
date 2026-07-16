d793d3568ef1d35b0d0d3e81adbf927bf57e451e
Collin Orendorff <co2fc@virginia.edu>
2026-07-16 16:26:01 -0400

    Finished layout and styling of cards block

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <header className="header">
             <img className="header__logo" src={logo} alt="Site Logo"/>
             <p className="header__date-and-location">DATE, LOCATION</p>
-            <button className="header__add-clothes-btn">+ Add Clothes</button>
+            <button className="header__add-clothes-btn">+ Add clothes</button>
             <div className="header__user-container">
                 <p className="header__username">Terrence Tegegne</p>
                 <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
